53f1e6b677dc51ac6c3a7bf02e1edd2ffe43c08e
LordNikon <100640945+wutangdev@users.noreply.github.com>
2023-03-03 17:34:02 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer: React.FC = () => {
-  return <footer className="text-left py-6 font-medium"><a href="https://github.com/wutangdev/wallet-tracker"> Check out the code here. </a></footer>;
+  return <footer className="text-left sm:text-center pl-3 py-6 font-medium"><a href="https://github.com/wutangdev/wallet-tracker"> Check out the code here. </a></footer>;
 };
 
 export default Footer;
